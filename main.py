952c3dc35af2d09b4e5a117f578c485dc3328d5b
Dmitry Parfenov <140505581+dimapar@users.noreply.github.com>
2024-09-16 22:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def send_welcome(message):
     bot.send_message(
         message.chat.id,
         """\
-Привет, я бот, помогающий записывать задачи/идеи в Notion через телеграм!\
+Hello, I'm a bot that helps you write down tasks/ideas in Notion via telegram!\
 """,
     )
 
@@ -26,19 +26,19 @@ def send_welcome(message):
 def echo_message(message):
     new_page = {
         "Name": {"title": [{"text": {"content": f"{message.text}"}}]},
-        "Список": {
+        "List": {
             "id": "Yn%40W",
             "type": "select",
             "select": {
                 "id": "12c077de-940d-4104-9ce5-005e60a68bec",
-                "name": "Корзина",
+                "name": "Trash",
                 "color": "gray",
             },
         },
     }
     notion.pages.create(parent={"database_id": DATA_BASE_API}, properties=new_page)
     bot.send_message(
-        message.chat.id, f"Следующая задача была добавлена в корзину:\n'{message.text}'"
+        message.chat.id, f"The following task has been added to cart:\n'{message.text}'"
     )
 
 
